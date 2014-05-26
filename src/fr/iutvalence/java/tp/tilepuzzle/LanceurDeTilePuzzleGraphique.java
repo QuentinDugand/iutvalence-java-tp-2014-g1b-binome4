25f3f929a43971c900953886db1294a115c3513d
dugandq <dugandq@PC-B122-01.iut-valence.fr>
2014-05-26 15:12:14 +0200

    modifications

--- src/fr/iutvalence/java/tp/tilepuzzle/LanceurDeTilePuzzleGraphique.java
+++ src/fr/iutvalence/java/tp/tilepuzzle/LanceurDeTilePuzzleGraphique.java
@@ -3,19 +3,33 @@ package fr.iutvalence.java.tp.tilepuzzle;
 public class LanceurDeTilePuzzleGraphique
 {
 	
-	public interface IHM 
-	{
-		
-	}
-	
-	public interface Joueur 
-	{
-		
-	}
-	
-	public class LanceurDeTilePuzzle 
-	{
-		
-	}
+	public void IHM()
+	{	
+		public class AffichageConsole implements Affichage
 
+			@Override
+			public void afficherDemandePosition() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void afficherPlateau(Plateau plateau) {
+				// TODO Auto-generated method stub
+				
+			}
+
+	}
+	
+	public void Joueur; 
+	
+		public Position getPosition(int hauteurPlateau, int largeurPlateau) {
+			return null;
+		
+	}
+	
+	public void TilePuzzle()
+	{
+
+	}
 }
